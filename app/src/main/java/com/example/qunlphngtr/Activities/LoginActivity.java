a7c9d81d2cf947df33be19a33a5f83ec2d28c38c
Duy <duynnpd02638@fpt.edu.vn>
2019-11-20 21:46:11 +0700

    test

--- app/src/main/java/com/example/qunlphngtr/Activities/LoginActivity.java
+++ app/src/main/java/com/example/qunlphngtr/Activities/LoginActivity.java
@@ -12,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void  initView() {
     }
 }
